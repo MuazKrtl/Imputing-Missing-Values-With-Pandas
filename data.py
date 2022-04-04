85ebfa69f9132b451d943d7a473c93daa0214c8f
Muaz Kartal <75174636+MuazKrtl@users.noreply.github.com>
2022-04-04 18:52:20 +0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import csv
+data = pd.read_csv('country_vaccination_stats.csv')
+
+countries = []
+dfs = []
+with open('country_vaccination_stats.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count != 0:
+            if row[0] not in countries:
+                countries.append(row[0])
+        if line_count == 0:
+            line_count += 1
+            
+print(countries)
+for country in range(len(countries)):
+    dfs.append(data[data['country'] == countries[country]])
+
+for df in dfs:
+    if df.shape[0] == 1:
+        df["daily_vaccinations"] = df["daily_vaccinations"].fillna(0)
+    else:
+        df["daily_vaccinations"] = df["daily_vaccinations"].fillna(df["daily_vaccinations"].min())
+
+dataframe = pd.concat(dfs)
+dataframe.to_csv("country_vaccination_stats.csv",index=False, sep=',',encoding='utf-8')
+
